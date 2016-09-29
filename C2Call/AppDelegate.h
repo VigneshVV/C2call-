9031b196e857cbbe5b1d5045bb740a9f76366d77
VigneshVV <vignesh0177@gmail.com>
2016-09-29 10:47:14 +0530

    Initial Commit

---
+++ C2Call/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  C2Call
+//
+//  Created by Vignesh on 29/09/16.
+//  Copyright © 2016 Naga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ C2Call/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  C2Call
+//
+//  Created by Vignesh on 29/09/16.
+//  Copyright © 2016 Naga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
